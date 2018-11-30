77c2e3d41bd64fceed5d93ab6a9831c619dc6e9f
crank <crank19869@gmail.com>
2018-11-30 09:51:30 +0600

    mini design news view

--- application/views/template/simple/news.php
+++ application/views/template/simple/news.php
@@ -9,6 +9,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         display: block;
     }
 </style>-->
+  <style>
+   .leftimg {
+    float:left; /* Выравнивание по левому краю */
+    margin: 7px 7px 7px 0; /* Отступы вокруг картинки */
+   }
+   .rightimg  {
+    float: right; /* Выравнивание по правому краю  */ 
+    margin: 7px 0 7px 7px; /* Отступы вокруг картинки */
+   }
+  </style>
 
 <div class="container theme-showcase" role="main">
     <?php if (isset($error_message)): // вывод ошибки если счет не на оплату найденхотя можно и show_error в контороллере  ?>
@@ -22,11 +32,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <?php foreach ($messages as $message): ?>
                 <div class="well">
                     <h3 align="center"> <?php echo $message->CaptionRu; ?></h3>
-                    <p align="center"> 
-                        <img src="<?php echo $message->Illustration; ?>" style = "width: 50%; height:50%">
-                    </p>
+                    <img src="<?php echo $message->Illustration; ?>" style = "width: 25%; height:25%" class="leftimg"> <!--верстка-->
                     <?php echo $message->ContentRu; ?>
-                    <p align="right"><?php echo $message->Date; ?></p>
+                    <p align="right">
+                        <?php echo $message->Date; ?>
+                    </p>       
                 </div>
             <?php endforeach; ?>
             <ul class="pagination">
