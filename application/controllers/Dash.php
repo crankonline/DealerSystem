d32af7b689404f26ad83897fc740a1083060dbda
crank <crank19869@gmail.com>
2018-12-14 11:03:48 +0600

    Revert "Merge branch 'feature/BugFix' into Development"
    This reverts commit 3d717c1532beb0b5a459f8d1cecc7c4b0f4a525a.

--- application/controllers/Dash.php
+++ application/controllers/Dash.php
@@ -21,11 +21,10 @@ class Dash extends CI_Controller {
     //private $per_page_ad = 1;
     //private $status_ad = '1';
 
-    private function pagination_gen($per_page, $Count = null) {
-        ($Count == NULL) ? $config['base_url'] = base_url() . 'index.php/dash/messages/' :
-                        $config['base_url'] = base_url() . 'index.php/dash/news/';
+    private function pagination_gen($per_page) {
+        $config['base_url'] = base_url() . 'index.php/dash/main_view/';
         $config['per_page'] = $per_page;
-        $config['num_links'] = 9;
+        $config['num_links'] = 10;
         $config['num_tag_open'] = '<li>';
         $config['num_tag_close'] = '</li>';
         $config['cur_tag_open'] = '<li class="active"><a href="#">';
@@ -40,17 +39,16 @@ class Dash extends CI_Controller {
         $config['last_tag_close'] = '</li>';
         $config['first_tag_open'] = '<li>';
         $config['first_tag_close'] = '</li>';
-        ($Count == NULL) ? $config['total_rows'] = $this->messages_model->record_count() :
-                        $config['total_rows'] = $Count;
+        $config['total_rows'] = $this->messages_model->record_count();
         $this->pagination->initialize($config);
         return $this->pagination->create_links();
     }
 
     public function index() {
-        redirect(base_url() . 'index.php/dash/news/'); //потомучто индекс нехочет с сегментами url работать
+        redirect(base_url() . 'index.php/dash/main_view/'); //потомучто индекс нехочет с сегментами url работать
     }
 
-    public function messages() {
+    public function main_view() {
         try {
             $data['pagination_message'] = $this->pagination_gen($this->per_page_messages);
             //$data['pagination_ad'] = $this->pagination_gen($this->status_ad, $this->per_page_ad);
@@ -61,7 +59,7 @@ class Dash extends CI_Controller {
         }
         $this->load->view('template/header');
         $this->load->view('template/menu', $this->session->userdata['logged_in']); //взависимости от авторизации
-        $this->load->view('template/simple/messages', $data); //в зависимости от авторизации (может и не надо)
+        $this->load->view('template/simple/main', $data); //в зависимости от авторизации (может и не надо)
         $this->load->view('template/footer');
     }
 
@@ -69,27 +67,10 @@ class Dash extends CI_Controller {
         try {
             $message = nl2br($this->input->post('message'));
             (!empty($message)) ? $this->messages_model->create_message($message) : NULL; //empty не фурычит
-            redirect(base_url() . 'index.php/dash/messages/');
+            redirect(base_url() . 'index.php/dash/');
         } catch (Exception $ex) {
             show_error($ex->getMessage(), 500, 'Ошибка при сохранении поста'); //на прод не работает
         }
-    }
-
-    public function news() {
-        try {
-            //var_dump(file_get_contents('http://dostek.kg/api/?action=News.getList&limit=5&offset='.$this->uri->segment(3)));die;
-            $Json = json_decode(file_get_contents('http://dostek.kg/api/?action=News.getList&limit=5&offset=' . $this->uri->segment(3)));
-            $data['messages'] = $Json->data;
-            $data['pagination_message'] = $this->pagination_gen($this->per_page_messages, $Json->count);
-            //var_dump($Json);die;
-        } catch (Exception $ex) {
-            $data['errormessage'] = $ex->getTraceAsString;
-        }
-
-        $this->load->view('template/header');
-        $this->load->view('template/menu', $this->session->userdata['logged_in']); //взависимости от авторизации
-        $this->load->view('template/simple/news', $data); //взависимости от авторизации
-        $this->load->view('template/footer');
     }
 
 }
