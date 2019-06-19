e2befb7f15cb7131407a4745e8a081318089556e
crank <crank19869@gmail.com>
2019-06-19 12:45:29 +0600

    update migration

--- application/migrations/003_clear_ci_sessions.php
+++ application/migrations/003_clear_ci_sessions.php
@@ -2,21 +2,14 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Add_field_ci_sessions extends CI_Migration {
+class Migration_Clear_ci_sessions extends CI_Migration {
 
     public function up() {
-        $field = array(
-            'cur_timestamp' => array(
-                'type' => 'TIMESTAMP',
-                'default' => 'NOW()',
-                'null' => TRUE
-            )
-        );
-        $this->dbforge->add_column('"Dealer_data".ci_sessions', $field);
+        $this->db->empty_table('Dealer_data".ci_sessions');
     }
 
     public function down() {
-        $this->dbforge->drop_column('"Dealer_data".ci_sessions', 'cur_timestamp');
+
     }
 
 }
