7d844909a6147ca0c180ecd37e7a9c9959e813c5
crank <crank19869@gmail.com>
2019-06-20 14:38:30 +0600

    output stream

--- application/controllers/Pdfcreate.php
+++ application/controllers/Pdfcreate.php
@@ -218,10 +218,11 @@ class Pdfcreate extends CI_Controller {
                     //$pdf = $this->pdf->load();
                     //$pdf->SetFooter($_SERVER['HTTP_HOST'] . '|{PAGENO}|' . date(DATE_RFC822)); // Add a footer for good measure ;)
                     $this->pdf->WriteHTML($html); // write the HTML into the PDF
-                    $this->pdf->Output($pdfFilePath, 'F'); // save to file because we can
+                    //$this->pdf->Output($pdfFilePath, 'F'); // save to file because we can
+                    $this->pdf->Output();
                 }
 
-                redirect("/downloads/$filename.pdf");
+                //redirect("/downloads/$filename.pdf");
             }
         } else {
             echo "no print";
