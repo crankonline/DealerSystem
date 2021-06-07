f1c053bb6272c2c7c4ea00e56056ff2beb084926
Mr. Crank <crank19869@gmail.com>
2021-06-07 17:41:28 +0600

    reFUCKtoring && feature

--- application/controllers/Requisites.php
+++ application/controllers/Requisites.php
@@ -515,8 +515,10 @@ class Requisites extends CI_Controller
             $postdate = file_get_contents("php://input");
             $request = json_decode($postdate);
             $Dumpfile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $request->file_ident;
-            file_put_contents($Dumpfile, fopen(getenv('MEDIA_SERVER') . 'file/download/' . $request->file_ident .
-                '?w=350&h=&f=center', 'r'));
+            $imageUrl = isset($request->large) ?
+                getenv('MEDIA_SERVER') . 'file/download/' . $request->file_ident
+                : getenv('MEDIA_SERVER') . 'file/download/' . $request->file_ident . '?w=350&h=&f=center';
+            file_put_contents($Dumpfile, fopen($imageUrl, 'r'));
             echo 'data:image/jpeg;base64,' . base64_encode(file_get_contents($Dumpfile));
         } catch (Exception $ex) {
             \Sentry\captureException($ex);
---
+++ resources/js/DealerSystem/RequisitesShowController.js
@@ -0,0 +1,48 @@
+app.controller('RequisitesShowController', ['$scope', '$uibModal', '$log', '$http', function ($scope, $uibModal, $log, $http) {
+    //window.scope = $scope;
+
+    let pc = this;
+    pc.data = {
+        Text: 'Сканированное изображение',
+        Image: ''
+    };
+
+    $scope.showImage = function (file_ident) {
+        $http.post('/index.php/requisites/get_image_reference', {
+            file_ident: file_ident,
+            large: true
+        }).then(function (response) {
+            pc.data.Image = response.data;
+            let modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: url,
+                controller: 'ModalInstanceCtrl',
+                controllerAs: 'pc',
+                size: 'lg',
+                resolve: {
+                    data: function () {
+                        return pc.data;
+                    }
+                }
+            });
+            modalInstance.result.then(function () {
+            }, function () {
+                modalInstance.close();
+            });
+        }, function (response) {
+            console.log(response.data);
+        });
+    };
+}]);
+
+app.controller('ModalInstanceCtrl', function ($uibModalInstance, data) {
+    let pc = this;
+    pc.data = data;
+
+    pc.ok = function () {
+        pc.data = {};
+        $uibModalInstance.close();
+    };
+});
